dbaecf4fef8302563e4fa3cbccabf092edd70184
ReaganOtema <reaganotema2022@gmail.com>
2025-11-14 08:28:54 +0300

    git add app.js git commit -m "Fix: added view engine for Render" git push

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const app = express();
-const inventoryRoutes = require('./routes/inventory-routes');
 const path = require('path');
+const app = express();
+
+// View engine setup -------------- FIXED!
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Body parsing (needed for later weeks)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
+const inventoryRoutes = require('./routes/inventory-routes');
 app.use('/', inventoryRoutes);
 
 // 404 handler
@@ -20,5 +28,6 @@ app.use((err, req, res, next) => {
     res.status(500).render('errors/error', { message: 'Something went wrong!' });
 });
 
+// Render-ready PORT
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
