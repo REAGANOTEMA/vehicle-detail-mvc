810e35ea422fa96dc23b234eb70e8b902797838b
ReaganOtema <reaganotema2022@gmail.com>
2025-11-16 18:30:49 +0300

    Initial changes

--- routes/inventoryroutes.js
+++ routes/inventoryroutes.js
@@ -6,13 +6,13 @@
 const express = require('express');
 const router = express.Router();
 
-// Import the controller
+// Correct import (your file is lowercase)
 const inventoryController = require('../controllers/inventorycontroller');
 
 // Route to show vehicle detail
-router.get('/vehicle/:id', inventorycontroller.vehicle-detail);
+router.get('/vehicle/:id', inventoryController.vehicledetail);
 
 // Route to trigger intentional 500 error
-router.get('/trigger-error', inventorycontroller.triggerError);
+router.get('/trigger-error', inventoryController.triggerError);
 
 module.exports = router;
